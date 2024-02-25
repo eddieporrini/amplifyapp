a59f0bab2ad1ef3b8c04e172d196df3f8edf935c
eddieporrini <ecp0006@mix.wvu.edu>
2024-02-25 09:22:58 -0500

    removed quotation marks

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>"CS 230L"</h1>
-        <h2>"Section 002"</h2>
-          <p>"WVU ID: 800214110"</p>
-          <p>"Hi I am Eddie"</p>
+      <h1>CS 230L</h1>
+        <h2>Section 002</h2>
+          <p>WVU ID: 800214110</p>
+          <p>Hi I am Eddie</p>
     </div>
   );
 }
